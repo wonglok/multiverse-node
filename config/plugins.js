6a068299875b44e7007a6039245eea5b0abb0815
Wong Lok <wonglok@users.noreply.github.com>
2022-07-18 09:23:37 +0800

    any

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,26 @@
-module.exports = ({ env }) => ({
-  // ...
-  io: {
-    enabled: true,
-    config: {
-      IOServerOptions: {
-        cors: { origin: "http://localhost:3000", methods: ["GET"] },
-      },
-      contentTypes: {
-        message: "*",
-        chat: ["create"],
-      },
-      events: [
-        {
-          name: "connection",
-          handler: ({ strapi }, socket) => {
-            strapi.log.info(`[io] new connection with id ${socket.id}`);
-          },
+module.exports = ({ env }) => {
+  return {
+    // ...
+    io: {
+      enabled: true,
+      config: {
+        IOServerOptions: {
+          cors: { origin: "*", methods: ["GET"] },
         },
-      ],
+        contentTypes: {
+          message: "*",
+          chat: ["create"],
+        },
+        events: [
+          {
+            name: "connection",
+            handler: ({ strapi }, socket) => {
+              strapi.log.info(`[io] new connection with id ${socket.id}`);
+            },
+          },
+        ],
+      },
     },
-  },
-  // ...
-});
+    // ...
+  };
+};
